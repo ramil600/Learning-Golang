ea2e488fe9307c7321917d377839af4ccb750242
Ramil M <62811433+ramil600@users.noreply.github.com>
2020-12-30 11:36:11 +0300

    Create timecost_pipeline.go
    While studying the pipeline pattern of Go, implemented the mock cost of running the pipeline code vs. sequential code. We have the pipeline with three stages:
    multiply -> add -> multiply, which is fed by the channel of 4 integers. Each multiply step costs 3ms(time.Sleep) and add 4ms.
    The total cost of the pipeline will be 16ms, which is the total cost of the most expensive task here: add.

---
+++ timecost_pipeline.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	start := time.Now()
+
+//this is the stage of the pipeline closes  out channel when exits, triggers the functions down the
+//pipeline to stop reading from out channel, has done channel that triggers to exit if main exits
+	multiply := func(done <-chan interface{}, integers <-chan int, mult int) <-chan int {
+		out := make(chan int)
+
+		go func() {
+			defer close(out)
+
+			for i := range integers {
+
+				select {
+				case <-done:
+					return
+				case out <- i * mult:
+					time.Sleep(3 * time.Millisecond)
+				}
+
+			}
+
+		}()
+		return out
+	}
+  //similar stage of the pipeline as multiply, exept costs 4ms to run
+	add := func(done <-chan interface{}, integers <-chan int, add int) <-chan int {
+		out := make(chan int)
+
+		go func() {
+			defer close(out)
+
+			for i := range integers {
+
+				select {
+				case <-done:
+					return
+				case out <- i + add:
+					time.Sleep(4 * time.Millisecond)
+				}
+
+			}
+
+		}()
+		return out
+  }
+  
+	generator := func(integers ...int) <-chan int {
+		out := make(chan int)
+		go func() {
+			defer close(out)
+			for _, i := range integers {
+				out <- i
+			}
+		}()
+		return out
+	}
+
+	//generate the feed to our pipeline
+  inputs := generator(1, 2, 3, 4)
+
+  //avoid leaking of goroutine, close the channel when main func exits
+	done := make(chan interface{})
+	defer close(done)
+
+  //pipeline of three costly operations: add 4ms, multiply 3ms
+	outputs := multiply(done, add(done, multiply(done, inputs, 2), 1), 3)
+
+	for j := range outputs {
+		fmt.Println(j)
+	}
+	//calculate time since the start of the pipeline
+  dur := time.Since(start)
+  
+  //total time cost of our pipeline is only the total cost of 16ms, more efficient that sequential cost of 40ms
+	fmt.Println("Total cost of operation is: ", dur.Milliseconds(), "milliseconds.")
+}
