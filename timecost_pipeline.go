4cf227c2b0974d59853d4965f0dc18488e96b15e
Ramil M <62811433+ramil600@users.noreply.github.com>
2020-12-30 11:38:45 +0300

    Update timecost_pipeline.go

--- timecost_pipeline.go
+++ timecost_pipeline.go
@@ -9,50 +9,42 @@ func main() {
 
 	start := time.Now()
 
-//this is the stage of the pipeline closes  out channel when exits, triggers the functions down the
-//pipeline to stop reading from out channel, has done channel that triggers to exit if main exits
+	//this is the stage of the pipeline closes  out channel when exits, triggers the functions down the
+	//pipeline to stop reading from out channel, has done channel that triggers to exit if main exits
 	multiply := func(done <-chan interface{}, integers <-chan int, mult int) <-chan int {
 		out := make(chan int)
-
 		go func() {
 			defer close(out)
-
 			for i := range integers {
-
 				select {
 				case <-done:
 					return
 				case out <- i * mult:
 					time.Sleep(3 * time.Millisecond)
 				}
-
 			}
-
 		}()
 		return out
 	}
-  //similar stage of the pipeline as multiply, exept costs 4ms to run
+	
+  	//similar stage of the pipeline as multiply, exept costs 4ms to run
 	add := func(done <-chan interface{}, integers <-chan int, add int) <-chan int {
 		out := make(chan int)
-
 		go func() {
 			defer close(out)
-
 			for i := range integers {
-
 				select {
 				case <-done:
 					return
 				case out <- i + add:
 					time.Sleep(4 * time.Millisecond)
 				}
-
 			}
-
 		}()
 		return out
-  }
-  
+  	}
+  	
+	//generate the feed to our pipeline
 	generator := func(integers ...int) <-chan int {
 		out := make(chan int)
 		go func() {
@@ -64,22 +56,21 @@ func main() {
 		return out
 	}
 
-	//generate the feed to our pipeline
-  inputs := generator(1, 2, 3, 4)
+  	inputs := generator(1, 2, 3, 4)
 
-  //avoid leaking of goroutine, close the channel when main func exits
+  	//avoid leaking of goroutine, close the channel when main func exits
 	done := make(chan interface{})
 	defer close(done)
 
-  //pipeline of three costly operations: add 4ms, multiply 3ms
+  	//pipeline of three costly operations: add 4ms, multiply 3ms
 	outputs := multiply(done, add(done, multiply(done, inputs, 2), 1), 3)
 
 	for j := range outputs {
 		fmt.Println(j)
 	}
 	//calculate time since the start of the pipeline
-  dur := time.Since(start)
+  	dur := time.Since(start)
   
-  //total time cost of our pipeline is only the total cost of 16ms, more efficient that sequential cost of 40ms
+  	//total time cost of our pipeline is only the total cost of 16ms, more efficient that sequential cost of 40ms
 	fmt.Println("Total cost of operation is: ", dur.Milliseconds(), "milliseconds.")
 }
