37041ef37f333cf036f1e000bf5b9ef73b535701
Ramil M <62811433+ramil600@users.noreply.github.com>
2021-02-25 08:42:58 +0300

    Create deferexit.go

---
+++ deferexit.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func slowoperation() {
+
+  // You want to clock slow operation on entry and on exit. It is implemented here by defering the function that is returned by anonymous 
+  // function below. Please pay attention to two sets of paranthesis, first calls anonymous function second one is for return function call,
+  //  that is deferred. It will be eventually called on exit, meanwhile function itself will be called at entry.
+  defer func() func() {
+		start := time.Now()
+    // Executed when function slowoperation starts
+		fmt.Println("Started at",start)
+		return func() {
+      // Return function will be deferred until slowoperation is done.
+			fmt.Println("Finished after", time.Since(start))
+		}
+	}()()
+  // Imitating some slow job
+	time.Sleep(5 * time.Second)
+
+}
+
+func main() {
+	slowoperation()
+	fmt.Println("Bye bye")
+}
