0259e11a5346ab4848d231f91bfc12fbb3603918
Ramil Mirhasan <62811433+ramil600@users.noreply.github.com>
2021-06-08 09:43:10 +0300

    Create jsonmarshaling.go

---
+++ jsonmarshaling.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Message struct {
+	Name string
+	Body string `json:"body,omitempty"`
+	Time int64
+}
+
+type Person struct {
+	Name    string
+	Age     int64
+	Parents Parent
+}
+
+type Parent struct {
+	Name     string
+	Lastname string
+}
+
+func main() {
+	var m = Message{"Hi", "", 0}
+	byts, _ := json.Marshal(m)
+	fmt.Println(string(byts))
+	fmt.Println(m)
+	var m2 Message
+
+	var byts2 = []byte(`{"Name":"Hi","body":"Hello there","Time":234}`)
+	json.Unmarshal(byts2, &m2)
+	fmt.Println(m2)
+	fmt.Println(m2.Body)
+	fmt.Println(m2.Time)
+
+	json1 := []byte(`{"Name":"Wednesday","Age":6,"Parents":{"Name":"Gomez","Lastname": "Morticia"}}`)
+	var f interface{}
+	json.Unmarshal(json1, &f)
+	mymap := f.(map[string]interface{})
+
+	//fmt.Println(mymap)
+        var ap Person
+	var parent Parent
+	for k, v := range mymap {
+
+		switch vv := v.(type) {
+		case float64:
+			fmt.Println(k, "is a float64 with value of ", vv)
+		case string:
+			fmt.Println(k, "is a string with value of ", vv)
+		case []interface{}:
+			for i, j := range vv {
+				fmt.Println(i, "is member of array with value of", j)
+			}
+		case map[string]interface{}:
+			jstr, _ := json.Marshal(v)
+			json.Unmarshal(jstr, &parent)
+			fmt.Println("Parent:" ,parent)	
+		default:
+			fmt.Println(k, "not sure what was it:", v)
+		}
+
+	}
+
+	
+	json.Unmarshal(json1, &ap)
+	fmt.Println(ap)
+	fmt.Println(ap.Parents.Name)
+	fmt.Println(ap.Parents.Lastname)
+
+}
